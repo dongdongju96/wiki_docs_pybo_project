5c7754468a8856f6b52b92f235f353d6ca82dbd1
dongdongju96 <leedongju15@gmail.com>
2021-12-07 23:42:11 +0900

    Add blueprint

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,6 +1,15 @@
 from flask import Flask
-app = Flask(__name__) # __name__ = 모듈명 여기서는 pybo
 
-@app.route('/')
-def hello_pybo():
-    return "Hello, Pybo!"
+# create_app 함수가 애플리케이션 팩토리다.
+# app 객체를 전역으로 사용할 때 발생하는 문제를 예방
+def create_app():
+    app = Flask(__name__)
+
+    from .views import main_views
+    app.register_blueprint(main_views.bp)
+    #
+    # @app.route('/')
+    # def hello_pybo():
+    #     return 'Hello, Pybo!'
+
+    return app
